409d67ced7bc747a33f98dcf953a9e465925e87c
Nazmul Hasan <81980364+nazmul-hasan54@users.noreply.github.com>
2022-07-06 21:23:20 +0600

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.Win32;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Path = System.IO.Path;
+
+namespace WPF_App
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            List<string> city = new List<string>()
+            {
+                "Dhaka",
+                "Chattagram",
+                "Khulna",
+                "Barishal"
+            };
+            cmbSellCity.ItemsSource = city;
+        }
+
+        private void btnUploadImage_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == true)
+            {
+                itemPic.Source = new BitmapImage(new Uri(ofd.FileName));
+                //var tmpFileName = DateTime.Now.Minute + "-" + DateTime.Now.Second + Path.GetExtension(ofd.FileName);
+                var tmpFileName = Guid.NewGuid() + Path.GetExtension(ofd.FileName);
+                //txtFilePath.Text = tmpFileName;
+                var imagePath = System.IO.Path.Combine(Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), @"../..Assets\Images\") + tmpFileName);
+                //File.Copy(ofd.FileName, imagePath);
+            }
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Models.Seller s = new Models.Seller();
+                s.Id = Convert.ToInt32(txtSellId.Text);
+                s.Name = txtSellName.Text;
+                s.Address = txtSellAddress.Text;
+                s.City = cmbSellCity.Text;
+                s.Country = txtSellCountry.Text;
+                s.Phone = txtPhone.Text;
+                s.Email = txtSellEmail.Text;
+                
+                var newSeller = "{'Id':'" + s.Id + "','Name':'" + s.Name + "','Address':'" + s.Address + "','City':'" + s.City + "','Country':'" + s.Country + "','Phone':'" + s.Phone + "','Email':'" + s.Email + "'}";
+
+                var sellerJson = File.ReadAllText(@"seller.json");
+                var jsonObject = JObject.Parse(sellerJson);
+                var itemArr = jsonObject.GetValue("Seller") as JArray;
+                var seller = JObject.Parse(newSeller);
+                itemArr.Add(seller);
+
+                jsonObject["Seller"] = itemArr;
+                string jsonResult = Newtonsoft.Json.JsonConvert.SerializeObject(jsonObject, Newtonsoft.Json.Formatting.Indented);
+                File.WriteAllText(@"seller.json", jsonResult);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error occured: " + ex.Message.ToString());
+            }
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mw = new MainWindow();
+            mw.Show();
+            this.Close();
+        }
+
+        private void btnAddNewOrder_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void btnSeeSellerList_Click(object sender, RoutedEventArgs e)
+        {
+            ShowAllSeller showAllSeller = new ShowAllSeller();
+            showAllSeller.Show();
+            this.Close();
+        }
+    }
+}
---
+++ ShowAllSeller.xaml.cs
@@ -0,0 +1,124 @@
+﻿using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_App
+{
+    /// <summary>
+    /// Interaction logic for ShowAllSeller.xaml
+    /// </summary>
+    public partial class ShowAllSeller : Window
+    {
+        public ShowAllSeller()
+        {
+            InitializeComponent();
+        }
+        public static int id;
+        public static string name = "";
+        public static string address = "";
+        public static string city = "";
+        public static string country = "";
+        public static string phone = "";
+        public static string email = "";
+
+        private void GetSellerList()
+        {
+            var json = File.ReadAllText(@"seller.json");
+            var jObject = JObject.Parse(json);
+            if (jObject!=null) 
+            {
+                JArray seller = (JArray)jObject["Sellers"];
+                if (seller!=null) 
+                {
+                    List<Models.Seller> sellers = new List<Models.Seller>();
+                    foreach (var item in seller)
+                    {
+                        sellers.Add(new Models.Seller() { Id=Convert.ToInt32(item["Id"]), Name=item["Name"].ToString(), Address=item["Address"].ToString(), City=item["City"].ToString(), Country=item["Country"].ToString(), Phone=item["Phone"].ToString(), Email=item["Email"].ToString()});
+                    }
+                    lvSeller.ItemsSource = sellers;
+                }
+            }
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            GetSellerList();
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            var itemsJson = File.ReadAllText(@"seller.json");
+            try
+            {
+                var jObject = JObject.Parse(itemsJson);
+                JArray itemArr = (JArray)jObject["Seller"];
+                Button button = sender as Button;
+                Models.Seller itemId = button.CommandParameter as Models.Seller;
+                int iId = itemId.Id;
+                var itemToDelete = itemArr.FirstOrDefault(obj => obj["Id"].Value<int>() == iId);
+
+                MessageBox.Show("Are you sure to delete this Seller?");
+                itemArr.Remove(itemToDelete);
+
+                string output = Newtonsoft.Json.JsonConvert.SerializeObject(jObject, Newtonsoft.Json.Formatting.Indented);
+                File.WriteAllText(@"seller.json", output);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+            finally
+            {
+                MessageBox.Show("Data deleted successfully!!!");
+                
+            }
+        }
+
+        private void btnView_Click(object sender, RoutedEventArgs e)
+        {
+            Button b = sender as Button;
+            Models.Seller sell = b.CommandParameter as Models.Seller;
+
+            id = sell.Id;
+            name = sell.Name;
+            email = sell.Email;
+            phone = sell.Phone;
+            address = sell.Address;
+            city = sell.City;
+            country = sell.Country;
+
+            MainWindow mw = new MainWindow();
+            mw.Show();
+            this.Close();
+
+
+        }
+
+        private void btnAddNewSeller_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mw = new MainWindow();
+            mw.Show();
+            this.Close();
+        }
+
+        private void btnSeller_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mw = new MainWindow();
+            mw.Show();
+            this.Close();
+        }
+    }
+}
